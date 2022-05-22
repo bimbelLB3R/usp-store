93eaef027563e8c8838af22fc5626fe4bd95d29b
tabalong <ikhwchemist@gmail.com>
2022-05-23 07:19:40 +0800

    carousel

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -5,15 +5,15 @@ import Image from 'next/image';
 const dataSlider = [
   {
     id: 1,
-    url: 'https://images.unsplash.com/photo-1627225924765-552d49cf47ad?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687',
+    url: 'https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
   },
   {
     id: 2,
-    url: 'https://images.unsplash.com/photo-1598082942907-da7f91b2f0e3?ixlib=rb-1.2.1&raw_url=true&q=60&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700',
+    url: 'https://images.unsplash.com/photo-1622351772377-c3dda74beb03?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=948',
   },
   {
     id: 3,
-    url: 'https://images.unsplash.com/photo-1589902860314-e910697dea18?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687',
+    url: 'https://images.unsplash.com/photo-1620799139834-6b8f844fbe61?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072',
   },
 ];
 const Carousel = () => {
@@ -38,18 +38,18 @@ const Carousel = () => {
                 className="w-full"
                 src={item.url}
                 alt="/"
-                width="100%"
-                height="100%"
-                objectFit=""
+                width="20%"
+                height="20%"
+                objectFit="cover"
                 layout="fill"
               />
               <BsArrowLeftSquare
                 onClick={prevSlide}
-                className="absolute top-[50%] text-3xl text-gray-50 cursor-pointer left-10 z-50"
+                className="absolute top-[50%] text-5xl text-fuchsia-600 cursor-pointer left-10 z-50"
               />
               <BsArrowRightSquare
                 onClick={nextSlide}
-                className="absolute top-[50%] text-3xl text-gray-50 cursor-pointer right-10 z-50"
+                className="absolute top-[50%] text-5xl text-fuchsia-600 cursor-pointer right-10 z-50"
               />
             </div>
           )}
