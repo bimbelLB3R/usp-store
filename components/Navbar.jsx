1423cd21cdc7065da9b9b95eed05e1fd16cf5387
tabalong <ikhwchemist@gmail.com>
2022-05-23 10:19:01 +0800

    styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,14 @@ const Navbar = () => {
     <div>
       <div className="flex justify-between list-none pr-4 md:pr-16 pl-4 md:pl-16 pt-6 pb-6 bg-gray-900 text-gray-100 z-30">
         <Link href={`/`}>
-          <p className="text-gray-50 tracking-widest text-3xl">USP Store</p>
+          <div>
+            <p className="text-gray-50 tracking-widest text-3xl mb-2">
+              USP <span className="border-b-4 border-fuchsia-400">Store</span>
+            </p>
+            <p className="text-[12px] italic">
+              Kaos, percetakan dan jasa sablon
+            </p>
+          </div>
         </Link>
         <div className="flex space-x-3">
           {/* <li>Home</li>
